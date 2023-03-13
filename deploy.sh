fc943b0fb4eaa666921ee013e94956f5180e281e
BIgerMe <xiao3017@sina.com>
2023-03-13 15:55:51 +0800

    first

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+# 构建
+npm run build
+
+# 进入输出产物文件夹
+cd dist
+
+# 如果你要部署到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果你要部署在 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/BIgerMe/frontend.git master:gh-pages
+
+cd -
